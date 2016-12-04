7d524b5c68819a753f694d0ae65964e5d3102bd5
Brett Duncan <brettd314@gmail.com>
2016-12-03 23:15:33 -0600

    updated code

--- Tasks.c
+++ Tasks.c
@@ -3,42 +3,42 @@ task bringBoomHome() {
 	wait1Msec(320);
 	motor[lift] = 0;
 }
-task extendBoom() { //All
+task auto1() {
 	extendBoomInches(7, 127);
 }
-task either() { //ForwardBackward
+task auto2() {
 	//driveInches(4*sqrt(24*24*2), 127);
-	driveInches(3*24, -127);
+	driveInches(3*24, 127);
 	delay(1000);
-	driveInches(18, 127);
+	driveInches(18, -127);
 	startTask(bringBoomHome);
 }
-task rightTurn() {
-	driveInches(3*24, -127);
-	driveInches(2*24, 127);
-	turnDegrees(135, 127);
+task auto3() {
+	driveInches(3*24, 127);
+	driveInches(2*24, -127);
+	turnDegrees(90, 127);
 	startTask(bringBoomHome);
 	driveInches(2.5*24, 127);
 }
-task leftTurn() {
-	driveInches(3*24, -127);
-	driveInches(2*24, 127);
-	turnDegrees(-135, 127);
+task auto4() {
+	driveInches(3*24, 127);
+	driveInches(2*24, -127);
+	turnDegrees(-80, 127);
 	startTask(bringBoomHome);
-	driveInches(2.*24, 127);
+	driveInches(2.5*24, 127);
 }
 task forwardBackward() {
 	extendBoomInches(7, 127);
-	startTask(extendBoom);
-	startTask(either);
+	startTask(auto1);
+	startTask(auto2);
 }
 task forwardBackwardCubeLeftStart() {
 	extendBoomInches(7, 127);
-	startTask(extendBoom);
-	startTask(rightTurn);
+	startTask(auto1);
+	startTask(auto3);
 }
 task forwardBackwardCubeRightStart() {
-	extendBoomInches(6.5, 127);
-	startTask(extendBoom);
-	startTask(leftTurn);
+	extendBoomInches(7, 127);
+	startTask(auto1);
+	startTask(auto4);
 }
